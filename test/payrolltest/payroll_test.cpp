94181b4ed8af13f9adb5452ad33e8dd38453bc3c
alfonsosalinas2 <31940640+alfonsosalinas2@users.noreply.github.com>
2017-09-20 18:57:49 -0500

    initial test for payroll_test.cpp
    added test function for get hourly regular pay

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,1 +1,12 @@
+#include "gtest/gtest.h"
+#include "Payroll.h"
 
+#type code to create two unit tests
+TEST(payrollTests, test_get_regular_pay_if_hours_40_pay_rate_25){
+    EXPECT_EQ(1000, getHourlyRegularPay(40,25));
+}
+TEST(payrollTests, test_get_regular_pay_if_hours_50_pay_rate_20){
+    EXPECT_EQ(1000, getHourlyRegularPay(50,20));
+}
+
+#EXPECT_EQ(expectedValue, actualValue);
