81a8a2c6f31191005309f63a2e73a3a35277b508
alfonsosalinas2 <31940640+alfonsosalinas2@users.noreply.github.com>
2017-09-20 18:31:47 -0500

    added payroll.h
    included all function prototypes

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,7 +1,22 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-
-
+string getEmployeeName();
+int getEmploymentCategory();
+int getTotalHours(int regular_hours, int ot_hours);
+int getYearlySalary();
+int getOtHours();
+double getPayRate();
+double getGrossPay(double gross_pay, double ot_pay);
+double getSalaryRegularPay(int yearly_salary, double BI_WEEKLY);
+double getHourlyRegularPay(int HOURS, double pay_rate);
+double getFIT(double gross_pay, double FIT_RATE);
+double getOtRate(double pay_rate, double OT_RATE_PERCENT);
+double getOtPay(int ot_hours, double ot_rate);
+double getFICASSN(double gross_pay, double FICA_SS_RATE);
+double getFICAMED(double gross_pay, double FICA_MED_RATE);
+double getNetPay(double gross_pay, double fica_med, double fica_ss, double fit);
+stringstream stringPayroll(string name, int total_hours, int ot_hours, double pay_rate, double ot_rate, double regular_pay, double ot_pay, double gross_pay, double fica_ss, double fica_med, double fit, double net_pay);
+void printPayroll(string output);
 
 #endif
