acba7d1089842467da04ffb3373594c9004c39ba
alfonsosalinas2 <31940640+alfonsosalinas2@users.noreply.github.com>
2017-09-20 20:03:12 -0500

    removed second comment

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -7,5 +7,3 @@ TEST(payrollTests, test_get_regular_pay_if_hours_40_pay_rate_25){
 TEST(payrollTests, test_get_regular_pay_if_hours_50_pay_rate_20){
     EXPECT_EQ(1000, getHourlyRegularPay(50,20));
 }
-
-#EXPECT_EQ(expectedValue, actualValue);
