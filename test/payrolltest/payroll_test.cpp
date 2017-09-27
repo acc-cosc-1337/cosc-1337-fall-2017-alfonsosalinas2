04f2be5d8dae421c9781f508f7d0c53741c33da8
alfonsosalinas2 <31940640+alfonsosalinas2@users.noreply.github.com>
2017-09-27 18:27:33 -0500

    update getSalaryRegularPay unit test

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -8,5 +8,5 @@ TEST(payrollTests, test_get_regular_pay_if_hours_50_pay_rate_20){
     EXPECT_EQ(1000, getHourlyRegularPay(50,20));
 }
 TEST(payrollTests, test_get_salary_regular_pay_50k_bi_weekly){
-    EXPECT_EQ(1923.076923076923, getSalaryRegularPay(50,20));
+    EXPECT_EQ(1923.076923076923, getSalaryRegularPay(50000,26));
 }
